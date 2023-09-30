dd08ef000a9a64c887e27dc43cc3eb6a2e8f2dd2
Tony Vargas <tavargas9@gmail.com>
2023-09-29 22:38:56 -0600

    Add footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,3 @@ fetch(requestUrl)
 
 var dropdownBtn = document.querySelector("#dropdown-btn");
 var dropdownContent = document.querySelector("#dropdown-content");
-
-//Add dropdown functionality:
-dropdownBtn.addEventListener('click', showDropdownMenu);
-
-function showDropdownMenu(event) {
-    event.preventDefault();
-    if (dropdownContent.classList.contains('hidden')) {
-        dropdownContent.classList.remove('hidden');
-    } else {
-        dropdownContent.classList.add('hidden');
-    };
-};
