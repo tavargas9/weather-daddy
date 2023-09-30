1edbcf46fecb6e8b203dea604c5377c835258b62
Tony Vargas <tavargas9@gmail.com>
2023-09-29 18:59:30 -0600

    Add nav bar and hero sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,18 @@ fetch(requestUrl)
     .then(function(data) {
         console.log(data);
     });
+
+var dropdownBtn = document.querySelector("#dropdown-btn");
+var dropdownContent = document.querySelector("#dropdown-content");
+
+//Add dropdown functionality:
+dropdownBtn.addEventListener('click', showDropdownMenu);
+
+function showDropdownMenu(event) {
+    event.preventDefault();
+    if (dropdownContent.classList.contains('hidden')) {
+        dropdownContent.classList.remove('hidden');
+    } else {
+        dropdownContent.classList.add('hidden');
+    };
+};
